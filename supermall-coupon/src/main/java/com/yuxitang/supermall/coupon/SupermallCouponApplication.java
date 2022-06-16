2c3bf937e6515a384726d13576b07ccb0361c75b
Yuxi Tang <1311076274@qq.com>
2022-06-16 00:42:57 -0400

    ren-ren fast run error  6/16

--- supermall-coupon/src/main/java/com/yuxitang/supermall/coupon/SupermallCouponApplication.java
+++ supermall-coupon/src/main/java/com/yuxitang/supermall/coupon/SupermallCouponApplication.java
@@ -1,9 +1,13 @@
 package com.yuxitang.supermall.coupon;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.yuxitang.supermall.coupon.dao")
 public class SupermallCouponApplication {
 
     public static void main(String[] args) {
